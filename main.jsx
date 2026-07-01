7ac77ded8f65ca1e965fd72c1adba66ef93148c1
cinar-99 <akcaycinar22@gmail.com>
2026-07-01 17:10:02 +0300

    Initialize main.jsx with React and SalesTab

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SalesTab from './SalesTab.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SalesTab />
+  </React.StrictMode>,
+)
